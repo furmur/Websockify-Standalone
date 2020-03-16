850fd72edb637e10ddb4828ab22cd97c32f5f56b
Michael Furmur <m.furmur@gmail.com>
2020-03-16 22:38:54 +0200

    update testing snippets

--- src/websockify.c
+++ src/websockify.c
@@ -3,7 +3,7 @@
 #include "WebsocketBridge.h"
 
 /* python3 -m websockets ws://127.0.0.1:52525
- * nc -l 127.0.0.1 5000 */
+ * echo -n -e "\xde\xad\xbe\xaf" | nc -k -l 127.0.0.1 5000 */
 
 int main(int argc, char **argv)
 {
